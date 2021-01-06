a7e95a7311c3032ab4161dd3447c820066ccb4df
Marika <>
2021-01-06 14:40:55 -0700

    Built activity class

---
+++ Activity.js
@@ -0,0 +1,23 @@
+class Activity {
+    constructor(category, description, minutes, seconds) {
+        this.category = category
+        this.description = description
+        this.minutes = minutes
+        this.seconds = seconds
+        this.completed = false
+        this.id = null
+    }
+    countdown() {
+
+    }
+
+    markComplete() {
+
+    }
+
+    saveToStorage() {
+
+    }
+}
+
+module.exports = Activity;
